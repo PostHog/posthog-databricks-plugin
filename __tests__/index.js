bafe53883122f030134ed3c8e048d1c0f262f277
samcaspus <sandeepguptan1998@gmail.com>
2022-04-18 22:18:58 +0530

    updated testcase logic

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,7 +3,7 @@ const { expect, test } = require('@jest/globals')
 const { transformEventToRow, exportEvents } = require('../index')
 
 test('vaildate unmarshal of transform event to row', () => {
-    let jsonObject = {
+    const jsonObject = {
         event: '$autocapture',
         properties: { props: 'is awesome 1998', $elements: { awrsomfseds: 'awesome' } },
         $set: 'set',
@@ -16,7 +16,7 @@ test('vaildate unmarshal of transform event to row', () => {
         uuid: 'uuid',
     }
 
-    let response = transformEventToRow(jsonObject)
+    const response = transformEventToRow(jsonObject)
     expect(response.event).toEqual('$autocapture')
     expect(response.distinct_id).toEqual('distinct id')
     expect(response.team_id).toEqual('team id')
@@ -25,12 +25,18 @@ test('vaildate unmarshal of transform event to row', () => {
 })
 
 test('export event and generate array for csv', async () => {
-    let global = {
+    const global = {
         eventsToIgnore: ['$autocapture'],
         tests: true,
     }
-    let config = {}
-    let events = ['$autocapture', '$pageview']
-    let result = await exportEvents(events, { global, config })
-    console.log(result)
+    const events = ['$autocapture', '$pageview']
+    const storageObject = {}
+    const mockedStorage = {
+        get: (key, none) => storageObject[key] || none,
+        set: (key, value) => {
+            storageObject[key] = value
+        },
+    }
+    const result = await exportEvents(events, { global, mockedStorage })
+    expect(result.length).toEqual(1)
 })
